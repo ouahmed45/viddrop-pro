3a58c605b6af851d047ef7bf48a145a7e31a7732
ouahmed45 <NAFAPUB@GMAIL.COM>
2026-09-16 00:29:21 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -96,6 +96,11 @@ def handle_info(query):
             'no_warnings': True,
             'noplaylist': True,
             'socket_timeout': 15,
+            'extractor_args': {
+                'youtube': {
+                    'player_client': ['android', 'ios'],
+                }
+            },
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
             }
@@ -170,6 +175,11 @@ def app(environ, start_response):
                 'no_warnings': True,
                 'socket_timeout': 30,
                 'retries': 5,
+                'extractor_args': {
+                    'youtube': {
+                        'player_client': ['android', 'ios'],
+                    }
+                },
                 'http_headers': {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
                 }
